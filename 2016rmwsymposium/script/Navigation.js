0ca45446ff207e9da2d2628d432a1c770b218a2d
Dave Landers <dave@srednal.com>
2018-01-26 07:25:46 -0700

    initial import from ftp

---
+++ 2016rmwsymposium/script/Navigation.js
@@ -0,0 +1,24 @@
+function presenters(){
+   location.href = "presenter2016.htm";
+}
+
+function program(){
+   location.href = "program2016.htm";
+}
+
+function gallery(){
+   location.href = "gallery.htm";
+}
+
+function vendorList(){
+   location.href = "vendors2016.htm";
+}
+
+
+function accomidations(){
+   location.href = "accommodations.htm";
+}
+
+function application(){
+   location.href = "application.htm";
+}
