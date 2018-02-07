593d194ab49f36e9c169d7ad1b0bb779e41a0cc4
Dave Landers <dave.landers@solidfire.com>
2018-02-07 10:55:37 -0700

    extract demo text and images to archives

---
+++ archives/demos/images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+dirs=$(ls|grep -v '\.')
+
+for d in $dirs; do
+  pushd $d
+
+    cat info.html | tr '<>' '\n\n'  | sed -n -e 's|^.*src="\([^"]*\)".*$|../../../2016rmwsymposium/\1|gp' > imgs
+
+    while read -r img; do
+      cp "$img" .
+      rm -f "$img"
+    done < imgs
+
+
+  popd
+done
