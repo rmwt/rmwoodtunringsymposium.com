2fdd9eecb5e388545cbcf48be285a6aedb71a555
Dave Landers <dave@srednal.com>
2018-02-07 21:10:01 -0700

    image sorting out

--- bin/trim.sh
+++ bin/trim.sh
@@ -5,13 +5,13 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 for ff in images/*.* images/demos/*.* images/pdf/*.* images/vendors/*.*; do
   f=$(basename "$ff")
 
-  if grep -q "$f" templates/*.*; then
+  if grep -q "$f" *.html; then
 
     echo $f in use
 
   else
 
-    echo ===============> remove $ff
+    echo =============== remove $ff
     # rm "$ff"
   fi
 done
